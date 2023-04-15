326ff96fc2ae11afdce1190970a14f11b21b1e81
Chollangi Harshitha <77778544+harshithach999@users.noreply.github.com>
2023-04-14 22:28:16 -0400

    Updated with pages

--- app.R
+++ app.R
@@ -1,15 +1,17 @@
 # Load libraries
-#Title : EDA on Breast Cancer
-#Team Members: "Shreya Kolanupaka, Harshitha Chollangi"
+# Title: EDA on Breast Cancer
+# Team Members: "Harshitha Chollangi, Shreya Kolanupaka"
 
+install.packages("shinydashboard")
+library(shinydashboard)
 library(shiny)
 library(ggplot2)
 
 # Read data
 breast_cancer <- read.csv("calc_case_description_train_set.csv")
-Breast_Cancer<-data.frame(breast_cancer)
+Breast_Cancer <- data.frame(breast_cancer)
 
-#setwd("harshithach999/CS-5610-100/calc_case_description_train_set.csv")
+# setwd("/BreastCancerAnalysis")
 
 # Define server function
 server <- function(input, output) {
@@ -18,43 +20,68 @@ server <- function(input, output) {
   filtered_data <- reactive({
     # Filter the breast cancer data based on the input fields
     filtered_data <- breast_cancer %>%
-      filter(assessment == input$assessment, pathology == input$pathology, subtlety %in% input$subtlety)
+      filter(assessment == input$assessment, pathology == input$pathology, calctype == input$calc.type)
     return(filtered_data)
   })
   
-  
-  #create plot for histogram
-  output$plot <- renderPlot({
+  # create plot for histogram on page 1
+  output$plot1 <- renderPlot({
     
     # Subset data based on input range
     subset_data <- Breast_Cancer[Breast_Cancer$pathology == input$pathology, ]
     
-    #create histogram
+    # create histogram
     ggplot(data = subset_data, aes(x = breast.density)) +
-      geom_histogram(binwidth = 1, fill = "blue", color = "white") +
+      geom_histogram(binwidth = 1, fill = "#fd7f67", color = "white") +
       labs(x = "Breast Density", y = "Frequency") +
       theme_minimal()
     
   })
   
+ 
+  
 }
 
-
 # Define UI function
-ui <- fluidPage(
+ui <- dashboardPage(
   
-  # Sidebar with input for pathology
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "pathology", label = "Pathology", choices = unique(Breast_Cancer$pathology))
-    ),
-    
-    # Main panel with plot
-    mainPanel(
-      plotOutput(outputId = "plot")
+  # Define dashboard header
+  dashboardHeader(title = "Breast Cancer Analysis"),
+  
+  # Define dashboard sidebar
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Page 1", tabName = "page1", icon = icon("dashboard")),
+      menuItem("Page 2", tabName = "page2", icon = icon("dashboard"))
     )
-  )
+  ),
   
+  # Define dashboard body
+  dashboardBody(
+    
+    # Define first page content
+    tabItems(
+      tabItem(tabName = "page1",
+              # Sidebar with input for pathology
+              sidebarPanel(
+                selectInput(inputId = "pathology", label = "Pathology", choices = unique(Breast_Cancer$pathology))
+              ),
+              
+              # Main panel with plot
+              mainPanel(
+                plotOutput(outputId = "plot1")
+              )
+      ),
+
+      
+      # Define third page content
+      tabItem(tabName = "page2",
+              h2("BreastCancer"),
+              tags$img(src="den.png"),
+              tags$p("Click", a(href="https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=22516629", "here"), "to read full article.")
+)
+)
+)
 )
 
 # Run Shiny app
